73598244ddab0f59fa2d2c2be90b5ec6676f2c79
rajnish-sharma <rajnishscrma@gmail.com>
2022-12-07 20:41:19 +0530

    to-do -list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+    let [inputText, setInputText] = useState("sss");
+   let [tasks, setTasks] = useState(["i am learning react", "for one week"]);
+
+    function changeHandler(event){
+        //console.log(event.target.value)
+        setInputText(event.target.value);
+       
+    }
+    
+    function submitHandler(e) {
+        e.preventDefault();
+        let tempTasks = [...tasks];
+        tempTasks.push(inputText);
+        console.log(tempTasks);
+        setTasks(tempTasks);
+
+        // clear input
+        setInputText("")
+    }
+
+    function deleteHandler(i){
+        //console.log(i);
+       let filtTasks =  tasks.filter((item, index) => {
+            if(index == i){
+                return false ;
+            }else{
+                return true ;
+            }
+        });
+
+       // console.log(filtTasks)
+       setTasks(filtTasks)
+    }
+
+    return <div>
+      {
+        tasks.map((item, index)=>{
+            return <div key={index}> <h1  style={{backgroundColor: "red"}}>{item}</h1>
+            <button onClick={() => deleteHandler(index)} >delete</button>
+            </div>
+            
+        })
+      }
+      
+        <form onSubmit={submitHandler}>
+            <input type="text" value={inputText}  placeholder="enter your task" onChange={changeHandler}></input>
+            <button type="submit">Add task</button>
+            
+        </form>
     </div>
-  );
 }
-
 export default App;
